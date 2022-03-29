f71fe3b905e19731dc9aab5e3280d83886c1bc2e
Seun <olagunjuoluwaseun1@gmail.com>
2022-03-30 00:25:05 +0100

    Initial commit

---
+++ src/main/java/com/oluwaseun/b2tech/B2techApplication.java
@@ -0,0 +1,13 @@
+package com.oluwaseun.b2tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2techApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(B2techApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oluwaseun/b2tech/B2techApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oluwaseun.b2tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class B2techApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
